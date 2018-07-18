603706ff819ab5f715571a9d779882be42cfc64d
cedric locchi <cedric.locchi@gmail.com>
2018-07-18 14:58:27 +0200

    update css et minification images/js/css

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const ugly = require('gulp-uglify');
+const clean = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const img = require('gulp-imagemin');
+
+gulp.task('css', () => {
+    return gulp.src('./css/*.css')
+        .pipe(clean({compatibility: 'ie8'}))
+        .pipe(rename('bundle.css'))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('js', () => {
+    return gulp.src('./js/*.js')
+        .pipe(ugly())
+        .pipe(rename('bundle.js'))
+        .pipe(gulp.dest('./js'));
+});
+
+gulp.task('img', ()=>{
+    return gulp.src(['./images/*.svg','./images/*.jpg'])
+        .pipe(img())
+        .pipe(gulp.dest('./images'));
+});
